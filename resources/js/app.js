e4179f30f4c33e5648d88ce5da6ca71b2b816650
softweb <softweb@localhost>
2025-11-21 12:06:17 +0100

    Add Breeze scaffolding files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,27 @@
-import '@primevue/themes/aura'        // tema PrimeVue (nuova API)
-import 'primeicons/primeicons.css'
+import '../css/app.css';
+import './bootstrap';
 
-import { createApp } from 'vue'
-import PrimeVue from 'primevue/config'
-import ToastService from 'primevue/toastservice'
-import axios from 'axios'
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-// CSRF per POST dal front-end (importantissimo per 419/route)
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
-axios.defaults.headers.common['X-CSRF-TOKEN'] =
-  document.querySelector('meta[name="csrf-token"]')?.getAttribute('content')
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-import CategoryTypesForm from './CategoryTypeForm.vue'
-
-const app = createApp(CategoryTypesForm)
-app.use(PrimeVue)
-app.use(ToastService)
-app.mount('#app')
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryTypeController;
+use Inertia\Inertia;
 
-Route::get('/category-types', [CategoryTypeController::class, 'create'])->name('category-types.create');
-Route::post('/category-types', [CategoryTypeController::class, 'store'])->name('category-types.store');
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
